08c9b046c2a133ff1b4dc66ba30d623f472a9ff8
nuit991 <161569652+nuit991@users.noreply.github.com>
2024-06-24 18:21:22 +0900

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -16,6 +16,10 @@ path('', home, name = 'home')
 1.其中path('', ...) 中的空字符串 '' 表示根路径，即网站的根 URL（例如 http://127.0.0.1:8000/）。
 2.当用户访问根 URL 时，这个路径会被匹配。
 
- path('api/gym-data/', gym_data_api, name = 'gym_data_api')
+path('api/gym-data/', gym_data_api, name = 'gym_data_api')
+->
+1.'api/gym-data/':單純一個名子的設定，用在網址，EX:http://127.0.0.1:8000/api/gym-data/
+2. gym_data_api:從views.py裡面抓gym_data_api這個函式
+3.name = 'gym_data_api'，寫這段之後就可以在其他地方使用，EX:home.html
 
 '''
