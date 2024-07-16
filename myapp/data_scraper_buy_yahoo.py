e1b811f9776f4dc3f38d4698270a1622d03fa6f2
nuit991 <161569652+nuit991@users.noreply.github.com>
2024-07-16 17:07:46 +0800

    Add files via upload

--- myapp/data_scraper_buy_yahoo.py
+++ myapp/data_scraper_buy_yahoo.py
@@ -31,7 +31,7 @@ def search_yahoo_product(product_name):
             search_box.send_keys(Keys.ENTER)
 
             # 使用 WebDriverWait 等待搜索結果加載
-            #WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.sc-1drl28c-1.cnHJYW')))
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.sc-1drl28c-1.cnHJYW')))
             
             # 獲取 HTML 內容
             html_content = driver.page_source
@@ -40,33 +40,41 @@ def search_yahoo_product(product_name):
             soup = BeautifulSoup(html_content, 'html.parser')
 
             # 找出所有符合條件的商品元素
-            product_elements = soup.find_all('a', class_='sc-1drl28c-1 cnHJYW') #先定位這條，Yahoo的這部分是最上層
-        
-            # 遍歷並處理每個商品元素
-            for product in product_elements[:10]:
-                # 提取商品名稱
-                prd_name = product.find('span', class_='sc-dlyefy sc-gKcDdr sc-1drl28c-5 jHwfYO ikfoIQ jZWZIY').text.strip()
-                
-                # 提取商品 URL
-                url = product['href']
-
-                # 提取商品價格
-                price = product.find('span', class_='sc-dlyefy sc-gKcDdr dfRcqf hFXgfs').text.strip()
-                
-                item_list.append((prd_name, url, price))
+            product_elements = soup.find_all('a', class_='sc-1drl28c-1 cnHJYW')
             
+            # 遍歷並處理每個商品元素
+            for product in product_elements:
+                try:
+                    # 提取商品名稱
+                    prd_name = product.find('span', class_='sc-dlyefy sc-gKcDdr sc-1drl28c-5 jHwfYO ikfoIQ jZWZIY').text.strip()
+                    # 提取商品 URL
+                    url = product['href']
+                    # 提取商品價格
+                    price = product.find('span', class_='sc-dlyefy sc-gKcDdr dfRcqf hFXgfs').text.strip()
+
+                    # 提取商品圖片
+                    img_tag = product.find('img')
+                    img_src = img_tag.get('src') if img_tag else None
+                    if img_src and img_src.endswith('.jpg'):
+                        item_list.append((prd_name, url, price, img_src))
+
+                except AttributeError as e:
+                    print(f"Error extracting product details: {str(e)}")
+
             driver.quit()
             return item_list
 
-        except AttributeError as e:
+        except Exception as e:
             print(f"Exception occurred: {str(e)}")
             attempt += 1
             print(f"Retry attempt {attempt}...")
             driver.quit()
 
-'''
+    print(f"Failed after {retries} retries. Exiting...")
+    return item_list
 
-product_name = '冷氣'
+'''
+product_name = '手機殼'
 results = search_yahoo_product(product_name)
 
 # 輸出搜索結果
@@ -74,6 +82,6 @@ for item in results:
     print(f"商品名稱: {item[0]}")
     print(f"連結: {item[1]}")
     print(f"價格: {item[2]}")
+    print(f"圖片: {item[3]}")
     print("-" * 20)
-
 '''
