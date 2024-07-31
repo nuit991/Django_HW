06da372f2a62e9a308aa44dc31079632d17eab1c
nuit991 <161569652+nuit991@users.noreply.github.com>
2024-07-31 17:09:32 +0800

    Add files via upload

--- myapp/data_scraper_buy_momo.py
+++ myapp/data_scraper_buy_momo.py
@@ -7,83 +7,99 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 
+#初始化瀏覽器
+def initialize_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+#進到momo商城，輸入商品名稱按下enter
+def search_product(driver, product_name):
+    driver.get("https://www.momoshop.com.tw/main/Main.jsp")
+    driver.implicitly_wait(10)
+    search_box = driver.find_element(By.XPATH, '//*[@id="keyword"]')
+    search_box.send_keys(product_name)
+    search_box.send_keys(Keys.ENTER)
+    time.sleep(10)  # 等待页面加载
+
+#用BeautifulSoup抓下整個網頁
+def get_page_content(driver):
+    html_source = driver.page_source
+    soup = BeautifulSoup(html_source, 'html.parser')
+    return soup
+
+#找到商品標籤，抓取名稱 / Url / 價錢 / 圖片
+def parse_product_info(soup):
+    item_list = []
+    item_containers = soup.find_all('li', attrs={'gcode': True})
+    for item_container in item_containers:
+        prd_name = item_container.find('h3', class_='prdName').text.strip()
+        price = item_container.find('span', class_='price').text.strip()
+        product_url_good = item_container.find('a', class_='goodsUrl')['href']
+        product_url = 'https://www.momoshop.com.tw' + product_url_good
+        img_tag = item_container.find('img', class_='prdImg')
+        img_url = img_tag['src']
+        item_list.append((prd_name, product_url, price, img_url))
+    return item_list
+
+#點下一頁
+def click_next_page(driver, current_page):
+    # 找到下一页按钮并点击
+    page_number = current_page
+    try:
+        #momo下一頁標籤的編號最多到11
+        #要先點下十頁(標籤數字是11)，才能到下10頁，不會點第10頁就自動顯示11、12、13頁...
+        if current_page <= 11:
+            css_selector = f'#BodyBase > div.bt_2_layout.searchbox.searchListArea.selectedtop > div:nth-child(6) > ul > li:nth-child({page_number}) > a'
+            element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+            element.click()
+            print(f'第{current_page} OK')
+            time.sleep(5)
+            
+            #這邊用page_number = current_page是為了第12頁網頁的標籤數字會變回4，之後丟給else處理
+            page_number = current_page
+        else:
+            #第12頁開始的規律都是(current_page % 10) + 2，current_page是我自己數的，網頁的標籤數字是4
+            page_number = (current_page % 10) + 2
+            css_selector = f'#BodyBase > div.bt_2_layout.searchbox.searchListArea.selectedtop > div:nth-child(6) > ul > li:nth-child({page_number}) > a'
+            # 使用 CSS 選擇器定位元素並點擊
+            element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+            element.click()
+            print(f'第{current_page} OK')
+            time.sleep(5)
+            WebDriverWait(driver, 20).until(EC.staleness_of(element))
+            
+
+    except Exception as e:
+        print(f"No more pages or error occurred: {str(e)}")
+
+
+#主要執行程式
 def search_momo_product(product_name, max_pages):
-    retries = 3  # 最大重试次数
+    retries = 5
     attempt = 0
     item_list = []
     total_items = 0
 
+    #若失敗的重複迴圈
     while attempt < retries:
         try:
-            chrome_options = Options()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-gpu")
-            chrome_options.add_argument("--log-level=3")
-            driver = webdriver.Chrome(options=chrome_options)
-            driver.get("https://www.momoshop.com.tw/main/Main.jsp")
-            driver.implicitly_wait(10)
-
-            # 使用 XPath 定位搜索框并输入商品名称
-            search_box = driver.find_element(By.XPATH, '//*[@id="keyword"]')
-            search_box.send_keys(product_name)
-            search_box.send_keys(Keys.ENTER)
-            time.sleep(10)
-            
-            html_source = driver.page_source
-            soup = BeautifulSoup(html_source, 'html.parser')
-
-            item_containers = soup.find_all('li', attrs={'gcode': True})
-            print(len(item_containers))
+            driver = initialize_driver()
+            search_product(driver, product_name)
             current_page = 1
-            page_number = current_page
-            page_number
-            while current_page <= max_pages: 
+            #User決定最大搜尋頁數
+            while current_page <= max_pages:
                 print(f"Scraping page {current_page}...")
+                soup = get_page_content(driver)
+                item_list.extend(parse_product_info(soup))  
+                click_next_page(driver, current_page)
+                current_page += 1
 
-                
-                for item_container in item_containers:
-                    prd_name = item_container.find('h3', class_='prdName').text.strip()
-                    #print(prd_name)
-                    price = item_container.find('span', class_='price').text.strip()
-                    #print(price)
-                    product_url_good = item_container.find('a', class_='goodsUrl')['href']
-                    product_url = 'https://www.momoshop.com.tw' + product_url_good
-                    #print(product_url)
-                    img_tag = item_container.find('img', class_='prdImg')
-                    img_url = img_tag['src']
-                    #print(img_url)
-                    
-
-                    item_list.append((prd_name, product_url, price, img_url))
-
-                # 找到下一页按钮并点击
-                try:
-                    if current_page <= 11:
-                        css_selector = f'#BodyBase > div.bt_2_layout.searchbox.searchListArea.selectedtop > div:nth-child(6) > ul > li:nth-child({page_number}) > a'
-                        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
-                        element.click()
-                        print(f'第{current_page} OK')
-                        time.sleep(5)
-                        current_page += 1
-                        page_number = current_page
-                    else:
-                        page_number = (current_page % 10) + 2
-                        css_selector = f'#BodyBase > div.bt_2_layout.searchbox.searchListArea.selectedtop > div:nth-child(6) > ul > li:nth-child({page_number}) > a'
-                        # 使用 CSS 選擇器定位元素並點擊
-                        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
-                        element.click()
-                        print(f'第{current_page} OK')
-                        time.sleep(5)
-                        WebDriverWait(driver, 20).until(EC.staleness_of(element))
-            
-                        current_page += 1
-
-                except Exception as e:
-                    print(f"No more pages or error occurred: {str(e)}")
-                    break
-
-            #print(f"Total 'Next Page' clicks: {current_page - 1}")  # 打印点击下一页次数
             total_items = len(item_list)
+            print(f"Page {current_page}: {total_items} products found.")
             return item_list, total_items
 
         except Exception as e:
@@ -93,16 +109,15 @@ def search_momo_product(product_name, max_pages):
 
         finally:
             driver.quit()
-            time.sleep(2)  # 等待2秒再重试
+            time.sleep(2)
 
     print("Reached maximum retries. Returning empty list.")
     return item_list, total_items
 
 '''
-
 # 要搜索的商品名称
 product_name = '冷氣'
-results, total_items = search_momo_product(product_name, max_pages=5)
+results, total_items = search_momo_product(product_name, max_pages=2)
 
 # 输出搜索结果
 for item in results:
@@ -111,4 +126,4 @@ for item in results:
     print(f"价格: {item[2]}")
     print(f"图片: {item[3]}")
     print("-" * 20)
-''' 
+'''
