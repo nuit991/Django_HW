5ac83b311bda6be70a6e097225bc5ec778698784
nuit991 <161569652+nuit991@users.noreply.github.com>
2024-07-31 15:57:29 +0800

    Add files via upload

--- myapp/data_scraper_buy_yahoo.py
+++ myapp/data_scraper_buy_yahoo.py
@@ -9,121 +9,193 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 
-def search_yahoo_product(product_name):
-    retries = 100  # 設定最大重試次數
+def setup_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # For headless mode
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+
+def search_product_on_yahoo(driver, product_name):
+    driver.get("https://tw.buy.yahoo.com/")
+    driver.implicitly_wait(10)
+    search_box = driver.find_element(By.XPATH, '//*[@id="UH_SAS"]/div[2]/div[2]/div/div/form/input[1]')
+    search_box.send_keys(product_name)
+    search_box.send_keys(Keys.ENTER)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.sc-1drl28c-1.cnHJYW')))
+
+    # 第1次更新權商品網址
+    current_url = driver.current_url
+
+    return current_url
+
+
+def scroll_to_bottom(driver):
+    scroll_step = 1000
+    while True:
+        driver.execute_script(f"window.scrollBy(0, {scroll_step});")
+        time.sleep(0.5)
+        if driver.execute_script("return window.innerHeight + window.scrollY") >= driver.execute_script("return document.body.scrollHeight"):
+            break
+
+
+def extract_product_info(driver, current_page):
+    html_content = driver.page_source
+    soup = BeautifulSoup(html_content, 'html.parser')
+    product_elements = soup.find_all('a', class_='sc-1drl28c-1 cnHJYW')
+    
+    # 打印当前页的产品数量
+    if current_page == 6:
+        print(f"Page {current_page}: {len(product_elements)} products found.")
+    else:
+        print(f"Page {current_page}: {len(product_elements)} products found.")
+    
+    product_info = []
+    for product in product_elements:
+        try:
+            # 提取商品 name 不要先+.text.strip()，不然他抓不到就會直接掛掉
+            prd_name_element = product.find('span', class_='sc-dlyefy sc-gKcDdr sc-1drl28c-5 jHwfYO ikfoIQ jZWZIY')
+            if prd_name_element:
+                prd_name = prd_name_element.text.strip()
+            else:
+                prd_name_element = product.find('span', class_='sc-gKcDdr sc-jMupca sc-1drl28c-5 kfLbyM hOVTkx jZWZIY')
+                if prd_name_element:
+                    prd_name = prd_name_element.text.strip()
+
+
+
+            # 提取商品 price
+            price_element = product.find('span', class_='sc-dlyefy sc-gKcDdr dfRcqf hFXgfs')
+            if price_element:
+                price = price_element.text.strip()
+            else:
+                price_element = product.find('span', class_='sc-gKcDdr sc-jMupca ETTiJ esZnNV')
+                if price_element:
+                    price = price_element.text.strip()
+
+    
+            # 提取商品 url
+            url = product['href']
+            #print('url', url)
+            img_urls = extract_product_images(driver, url)
+            #print('img_urls OK', img_urls)
+            product_info.append((prd_name, url, price, img_urls))
+        except AttributeError as e:
+            print(f"Error extracting product details: {str(e)}")
+        flattened_data = []
+        # 遍历原始数据
+        for item in product_info:
+            prd_name, url, price, img_urls = item
+            if isinstance(img_urls, list) and img_urls:
+            # 提取列表中的第一个图片 URL
+                img_url = img_urls[0]
+            else:
+                img_url = img_urls  # 如果已经是字符串，直接使用
+    
+            # 将扁平化后的数据项添加到新的列表中
+            flattened_data.append((prd_name, url, price, img_url))
+    return flattened_data
+
+
+def extract_product_images(driver, product_url):
+    img_urls = []
+    try:
+        driver.get(product_url)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        img_wrapper = soup.find('div', class_='LensImage__imgWrapper___SXnau')
+        if img_wrapper:
+            img_tags = img_wrapper.find_all('img')
+            img_urls = [img.get('src') for img in img_tags if img.get('src') and (img.get('src').endswith('.jpg') or img.get('src').endswith('.jpeg'))]
+            time.sleep(1)
+        else:
+            img_wrapper = soup.find_all('span', class_='ImageHover__thumbnail___1YTO5')
+            if img_wrapper:
+                img_tags = img_wrapper[1].find_all('img')
+                img_urls = [img.get('src') for img in img_tags if img.get('src') and (img.get('src').endswith('.jpg') or img.get('src').endswith('.jpeg'))]
+                time.sleep(1)
+    except Exception as e:
+        print(f"Failed to extract images for product URL: {product_url}")
+        print(f"Error: {str(e)}")
+    return img_urls
+
+
+def click_next_page(driver, current_page):
+    try:
+        if current_page == 4:
+            css_selector = f'#isoredux-root > div.page.shopping.disableUserSelectInTouchDevice.Layout_layoutBg_3R7Zt > div:nth-child(1) > div.pageFlex > div > div.Pagination_pagination_uC5J7 > div > div > a:nth-child({current_page})'
+        if current_page != 4 and current_page < 4:
+            css_selector = f'#isoredux-root > div.page.shopping.disableUserSelectInTouchDevice.Layout_layoutBg_3R7Zt > div:nth-child(1) > div.pageFlex > div > div.Pagination_pagination_uC5J7 > div > div > a:nth-child({current_page + 1})'
+        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+        element.click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.sc-1drl28c-1.cnHJYW')))
+        #第2次更新權商品網址
+        
+        return True
+    except Exception as e:
+        print(f"Failed to click on page {current_page + 1} button: {str(e)}")
+        return False
+
+
+def search_yahoo_product(product_name, max_page):
+    retries = 100
     attempt = 0
     item_list = []
     
     while attempt < retries:
         try:
-            chrome_options = Options()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-gpu")
-            chrome_options.add_argument("--log-level=3")
-            driver = webdriver.Chrome(options=chrome_options)
+            driver = setup_driver()
+            search_product_on_yahoo(driver, product_name)
+            current_page = 1
             
-            driver.get("https://tw.buy.yahoo.com/") # 更改網址以前往不同網頁
-            driver.implicitly_wait(10)
 
-            # 使用 XPath 定位到搜索欄位並輸入商品名稱
-            search_box = driver.find_element(By.XPATH, '//*[@id="UH_SAS"]/div[2]/div[2]/div/div/form/input[1]')
-            search_box.send_keys(product_name)
-            search_box.send_keys(Keys.ENTER)
-
-            # 使用 WebDriverWait 等待搜索結果加載
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.sc-1drl28c-1.cnHJYW')))
-            
-            # 獲取 HTML 內容
-            html_content = driver.page_source
-        
-            # 使用 BeautifulSoup 解析 HTML
-            soup = BeautifulSoup(html_content, 'html.parser')
-
-            # 找出所有符合條件的商品元素
-            product_elements = soup.find_all('a', class_='sc-1drl28c-1 cnHJYW')
-            
-            # 遍歷並處理每個商品元素
-            for product in product_elements:
-                try:
-                    # 提取商品名稱
-                    prd_name = product.find('span', class_='sc-dlyefy sc-gKcDdr sc-1drl28c-5 jHwfYO ikfoIQ jZWZIY').text.strip()
-                    # 提取商品 price
-                    price = product.find('span', class_='sc-dlyefy sc-gKcDdr dfRcqf hFXgfs').text.strip()
-                    # 提取商品 url
-                    url = product['href']
-                    # 提取商品 圖片
-                    #使用一個一個點進商品來抓的方式
-                    if url:
-                        try:
-                            driver.get(url) 
-                            html = driver.page_source
-                            soup = BeautifulSoup(html, 'html.parser')
-                            #print(soup)
-                            img_wrapper = soup.find('div', class_='LensImage__imgWrapper___SXnau')
-                            #print(img_wrapper)
-                            img_urls = []
-                            if img_wrapper:
-                                img_tags = img_wrapper.find_all('img')
-                                for img in img_tags:
-                                    img_url = img.get('src')
-                                    if img_url and img_url.endswith('.jpg'):
-                                        img_urls.append(img_url)
-
-                            if not img_urls:   # 這樣寫的話 img_urls 表示為empty or False，如果是寫 if img_urls == None則是只有表示為None
-                                img_wrapper = soup.find_all('span', class_='ImageHover__thumbnail___1YTO5')
-                                if img_wrapper:
-                                    img_tags = img_wrapper[1]
-                                    img_tags_1 = img_tags.find_all('img')
-                                    for img in img_tags_1:
-                                        img_url = img.get('src')
-                                        if img_url and img_url.endswith('.jpg'):
-                                            img_urls.append(img_url)
-                                            
-                            #print(img_tags)
-                            #print(img_urls)
-                            #print('\n')
-                        except AttributeError as e:
-                            print(f"Error extracting product details: {str(e)}")
-                        #product_span = product_img.find('span', class_='ImageHover__thumbnail___1YTO5')
-                        #print(product_span)
-                        # 提取商品價格
-    
-                    item_list.append((prd_name, url, price, img_urls))
-                except AttributeError as e:
-                    print(f"Error extracting product details: {str(e)}")
+            while current_page <= max_page:
+                #往下滑
+                scroll_to_bottom(driver)
+                    
+                #抓商品資料
+                product_info = extract_product_info(driver, current_page)
+                item_list.extend(product_info)
+                if current_page == 1:
+                    current_url =  search_product_on_yahoo(driver, product_name) #返回current_url(會再重複一次輸入商品名稱然後按下搜尋的動作)
+                    print(current_url)
+                    #回到全品商品的那一頁
+                    driver.get(current_url)
+                    #按下一頁
+                    click_next_page(driver, current_page)
+                    time.sleep(5)
+                    current_page += 1
+                    current_url = driver.current_url
+                else:
+                    driver.get(current_url)
+                    click_next_page(driver, current_page)  
+                    time.sleep(5) 
+                    #print(current_url)
+                    current_url = driver.current_url
+                    current_page += 1
 
             driver.quit()
-
-
-            #這邊處理[123,[ABC]]的問題
-            flattened_data = []
-            # 遍历原始数据
-            for item in item_list:
-                prd_name, url, price, img_urls = item
-                if isinstance(img_urls, list) and img_urls:
-                    # 提取列表中的第一个图片 URL
-                    img_url = img_urls[0]
-                else:
-                    img_url = img_urls  # 如果已经是字符串，直接使用
-    
-                # 将扁平化后的数据项添加到新的列表中
-                flattened_data.append((prd_name, url, price, img_url))
-
-            return flattened_data
+            len_item_list = len(item_list)
+            #print(len_item_list)
+            #print(item_list)
+            return item_list, len_item_list
 
         except Exception as e:
             print(f"Exception occurred: {str(e)}")
             attempt += 1
             print(f"Retry attempt {attempt}...")
             driver.quit()
-
+    
     print(f"Failed after {retries} retries. Exiting...")
-    return item_list
+    return item_list, len_item_list
 
 '''
-product_name = '手機殼'
-results = search_yahoo_product(product_name)
-
+# 測試搜索功能
+product_name = '牙刷'
+max_page = 2
+results, len_item_list = search_yahoo_product(product_name, max_page)
 
 # 輸出搜索結果
 for item in results:
