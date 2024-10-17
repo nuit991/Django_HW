75ca6f971ffa42d2bafe922ee3b9d20d54a89454
nuit991 <161569652+nuit991@users.noreply.github.com>
2024-10-17 13:34:21 +0900

    Update data_scraper_buy_momo.py

--- myapp/data_scraper_buy_momo.py
+++ myapp/data_scraper_buy_momo.py
@@ -52,9 +52,9 @@ def parse_product_info(soup):
         print('price', price)
 
         #product_url_good = item_container.find('div', class_='swiper-slide swiper-slide-active')
-        product_url = item_container.find('a', class_='goodsUrl')['href']
+        product_url_good = item_container.find('a', class_='goodsUrl')['href']
         #print('product_url_good', product_url_good)
-        #product_url = 'https://www.momoshop.com.tw' + product_url_good 
+        product_url = 'https://www.momoshop.com.tw' + product_url_good 
         #product_url = product_url_1
         print('product_url', product_url)
 
